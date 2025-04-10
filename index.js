414186f70703891640d0039521353e6e5e6d0529
comvn <145321168+comvn@users.noreply.github.com>
2025-04-10 09:39:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const { Logtail } = require("@logtail/js");
+import express from "express";
+import { Logtail } from "@logtail/js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
 const logtail = new Logtail('Ew4KHV2ZLcbekkEeE5sLrQQK');
 const port = process.env.PORT || 3000;
 

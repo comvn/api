d47d02e6c83ead326ebc2fd958923ece76e265c2
comvn <145321168+comvn@users.noreply.github.com>
2025-04-10 09:42:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import express from "express";
-import { Logtail } from "@logtail/js";
-import dotenv from "dotenv";
+require("dotenv").config();
+const express = require("express");
+const Logtail = require("@logtail/js").Logtail;
 
-dotenv.config();
+const logtail = new Logtail(process.env.LOGTAIL_TOKEN);
+
 const app = express();
-
-const logtail = new Logtail('Ew4KHV2ZLcbekkEeE5sLrQQK');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -19,15 +18,15 @@ app.use(async (req, res, next) => {
   };
 
   await logtail.info("Request received", logData);
-
   console.log(JSON.stringify(logData, null, 2));
+
   next();
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello with Logtail!");
+  res.send("Hello from Logtail-powered API!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
